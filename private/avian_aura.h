556ffe57761d879361246c58a87c5d43cce4ed80
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-03-24 23:03:43 +0530

    Create avian_aura.h

---
+++ private/avian_aura.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+// ? #include <ncurses.h>
+// ? #include <pbPlots.h>
+// ? #include <libcurl.h>
+// ? #include <sqlite3.h>
+
+#define FOR(i,n) for(int i=0; i<n; i++)
+
+// * considering a struct for user 
+typedef struct credential{
+    char username[12];
+    char password[20];
+    char email[100];
+    long long int mobile_number;
+    char security_question[100];
+    char security_answer[20];
+} credential;
