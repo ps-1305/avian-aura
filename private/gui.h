ae7e570f51da0d85984b5d7f8c34106fb9aafe25
Agam77055 <136053043+Agam77055@users.noreply.github.com>
2024-04-17 21:42:37 +0530

    Update gui.h

--- private/gui.h
+++ private/gui.h
@@ -4,8 +4,9 @@
 #ifndef GUI_H
 #define GUI_H
 void print_menu_1(WINDOW *win1, int h1);
-void print_menu_2(WINDOW *win2, int h2);
+void print_menu_2(WINDOW *win2, int x, int y);
 
+void start(WINDOW *win);
 void profile(WINDOW *win);
 void flights(WINDOW *win);
 void trains(WINDOW *win);
