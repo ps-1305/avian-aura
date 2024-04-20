1e24ae9402986018e228a8ce858177eda74878ee
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-04-20 19:00:46 +0530

    HEADER - Header files

---
+++ dependencies.h
@@ -0,0 +1,11 @@
+#ifndef DEPENDENCIES_H
+
+#include "signup.c"
+#include "forgot_password.c"
+#include "authenticateUser.c"
+#include "main.c"
+
+#define NEW_GTK_TOPLEVEL "new"
+#define DESTROY_OLD_TOPLEVEL "destroy"
+
+#endif
---
+++ secondary.h
@@ -0,0 +1,14 @@
+#ifndef SECONDARY_H
+
+#include <stdio.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <stdbool.h>
+
+#include "busTicket.c"
+#include "railTicket.c"
+
+#define PLACEHOLDER_ARGS "placeholder"
+#define SECONDARY_ARGS "secondary"
+
+#endif
