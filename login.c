0d1823b3febdf361c7c87f6e1b10075e50fe44e9
Prashant Srivastav <147681761+ps-1305@users.noreply.github.com>
2024-04-29 16:33:00 +0530

    Update login.c

--- login.c
+++ login.c
@@ -12,6 +12,12 @@ GtkWidget *signup_button;
 GtkWidget *forgot_password;
 GtkWidget *window;
 
+/*
+__CALLBACK__
+args = widget pointer and event pointer (G_CALLBACK)
+returns = gboolean (not of any use, only signifies whether a user has valid credentials or not)
+function = logs a user in using the login() function of authenticateUser.c
+*/
 gboolean login_attempt(GtkWidget *widget, GdkEvent *event){
   char* args[] = {NEW_GTK_TOPLEVEL, DESTROY_OLD_TOPLEVEL};
 
@@ -40,6 +46,12 @@ gboolean login_attempt(GtkWidget *widget, GdkEvent *event){
   }
 }
 
+/*
+__CALLBACK__
+args = widget pointer and event pointer (G_CALLBACK)
+returns = gboolean (only signifies whether a user has signed up or not)
+function = 
+*/
 gboolean signup(GtkWidget *widget, GdkEvent *event){
   gtk_main_quit();
   char* args[] = {NEW_GTK_TOPLEVEL, DESTROY_OLD_TOPLEVEL};
@@ -47,6 +59,13 @@ gboolean signup(GtkWidget *widget, GdkEvent *event){
   return TRUE;
 }
 
+/*
+__CALLBACK__
+args = widget pointer and event pointer (G_CALLBACK)
+returns = gboolean (not of any use)
+function = calls a function forgot_password_init (from forgot_password.c) and logs in a user if 
+the function returns back true (line 157 :: forgot_password.c)
+*/
 gboolean forgot_attempt(GtkWidget *widget, GdkEvent *event){
   char* args[] = {NEW_GTK_TOPLEVEL, DESTROY_OLD_TOPLEVEL};
   char security[20] = "**** ****";
@@ -61,9 +80,10 @@ gboolean forgot_attempt(GtkWidget *widget, GdkEvent *event){
 }
 
 int main(int argc, char *argv[]){
+  // __init__ 
   gtk_init(&argc, &argv);
 
-  // * photo
+  // * photos
   GdkPixbuf *logo = gdk_pixbuf_new_from_file("img/logo.png", NULL);
   GdkPixbuf *logo_scaled = gdk_pixbuf_scale_simple(logo, 200, 200, GDK_INTERP_BILINEAR);
   GtkWidget *image_logo = gtk_image_new_from_pixbuf(logo_scaled);
@@ -85,6 +105,7 @@ int main(int argc, char *argv[]){
   gtk_window_set_title(GTK_WINDOW(window), "Avian Aura : Login");
   gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
+  // ! gtk window can only handle 1 widget inside it, so we keep it a vertical box
   GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 7);
   gtk_container_add(GTK_CONTAINER(window), vbox);
 
@@ -103,8 +124,10 @@ int main(int argc, char *argv[]){
   gtk_container_add(GTK_CONTAINER(vbox), hbox_1);
   pad_from_all_sides(hbox_1, 20);
   
+  // label
   GtkWidget *username_label = gtk_label_new("Username: ");
   gtk_box_pack_start(GTK_BOX(hbox_1), username_label, FALSE, FALSE, 0);
+  // entry
   username_entry = gtk_entry_new();
   gtk_box_pack_start(GTK_BOX(hbox_1), username_entry, FALSE, FALSE, 0);
 
@@ -115,17 +138,19 @@ int main(int argc, char *argv[]){
   gtk_widget_set_margin_start(hbox_2, 20);
   gtk_widget_set_margin_end(hbox_2, 20);
 
+  // label
   GtkWidget *password_label = gtk_label_new("Password: ");
   gtk_box_pack_start(GTK_BOX(hbox_2), password_label, FALSE, FALSE, 0);
+  // entry
   password_entry = gtk_entry_new();
-  gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE);
+  gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE); // we set the visibility false for password
   gtk_box_pack_start(GTK_BOX(hbox_2), password_entry, FALSE, FALSE, 0);
 
   // 3. Login button
   login_button = gtk_button_new();
   gtk_button_set_image(GTK_BUTTON(login_button), image_btn_1);
-  gtk_button_set_always_show_image(GTK_BUTTON(login_button), TRUE);
-  g_signal_connect(login_button, "clicked", G_CALLBACK(login_attempt), NULL);
+  gtk_button_set_always_show_image(GTK_BUTTON(login_button), TRUE); // img
+  g_signal_connect(login_button, "clicked", G_CALLBACK(login_attempt), NULL); // calling our login function
   gtk_box_pack_start(GTK_BOX(vbox), login_button, FALSE, FALSE, 0);
   gtk_widget_set_margin_start(login_button, 20);
   gtk_widget_set_margin_end(login_button, 20);
@@ -133,8 +158,8 @@ int main(int argc, char *argv[]){
   // 4. Signup button
   signup_button = gtk_button_new();
   gtk_button_set_image(GTK_BUTTON(signup_button), image_btn_2);
-  gtk_button_set_always_show_image(GTK_BUTTON(signup_button), TRUE);
-  g_signal_connect(signup_button, "clicked", G_CALLBACK(signup), NULL);
+  gtk_button_set_always_show_image(GTK_BUTTON(signup_button), TRUE);  // img
+  g_signal_connect(signup_button, "clicked", G_CALLBACK(signup), NULL); // calling our signup function
   gtk_box_pack_start(GTK_BOX(vbox), signup_button, FALSE, FALSE, 0);
   gtk_widget_set_margin_start(signup_button, 20);
   gtk_widget_set_margin_end(signup_button, 20);
@@ -142,8 +167,8 @@ int main(int argc, char *argv[]){
   // 5. Forgot password  button
   forgot_password = gtk_button_new();
   gtk_button_set_image(GTK_BUTTON(forgot_password), image_btn_3);
-  gtk_button_set_always_show_image(GTK_BUTTON(forgot_password), TRUE);
-  g_signal_connect(forgot_password, "clicked", G_CALLBACK(forgot_attempt), NULL);
+  gtk_button_set_always_show_image(GTK_BUTTON(forgot_password), TRUE);  // img
+  g_signal_connect(forgot_password, "clicked", G_CALLBACK(forgot_attempt), NULL); // calling our forgot attempt function
   gtk_box_pack_start(GTK_BOX(vbox), forgot_password, FALSE, FALSE, 0);
   gtk_widget_set_margin_bottom(forgot_password, 20);
   gtk_widget_set_margin_start(forgot_password, 20);
@@ -151,8 +176,10 @@ int main(int argc, char *argv[]){
 
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-  // Show all widgets and run the main loop
+  // .showall()
   gtk_widget_show_all(window);
+
+  // .mainloop()
   gtk_main();
 
   return 0;
